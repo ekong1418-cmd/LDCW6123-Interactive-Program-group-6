a7b44cdaf93b8ce678686f8b83d4b30018341e23
Kong Eason <ekong1418@gmail.com>
2026-06-30 22:50:32 +0800

    Selection of vehicle

---
+++ vehicle selection.cpp
@@ -0,0 +1,10 @@
+ // Vehicle selectio
+    cout << "\nSelect Grab service:" << endl;
+    cout << "1. GrabBike" << endl;
+    cout << "2. GrabCar" << endl;
+    cout << "3. GrabPremium" << endl;
+
+
+    cout << "Enter your choice: ";
+    cin >> choice;
+
