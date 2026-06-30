c56d88451a9358a0705f778d198ce547414db6bc
Kong Eason <ekong1418@gmail.com>
2026-06-30 22:36:21 +0800

    Added main menu

---
+++ front welcome page.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    string name;
+    int distance;
+    int choice;
+    double fare;
+
+
+    cout << "===== Grab Fare Calculator System =====" << endl;
+
+
+    // Input customer name
+    cout << "Enter customer name: ";
+    cin >> name;
+
+
+    // Input distanc
+    cout << "Enter distance (km): ";
+    cin >> distance;
+
+
+
+
